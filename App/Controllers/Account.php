1ffd999bb8fef0be457ef72c270c104cae84ba40
Joseph Karl Magnussen <jkmagnussen@icloud.com>
2022-02-08 21:21:55 +0000

    edit user functionality changes

--- App/Controllers/Account.php
+++ App/Controllers/Account.php
@@ -17,7 +17,7 @@ use \App\Models\User;
       * @return void
       */
       public function validateEmailAction(){
-          $is_valid = ! User::emailExists($_GET['email']);
+          $is_valid = ! User::emailExists($_GET['email'], $_GET['ignore_id'] ?? null);
 
           header('Content-Type: application/json');
           echo json_encode($is_valid);
